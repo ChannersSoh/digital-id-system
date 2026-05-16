21050ae89e972da4396caaaa956eb102ba7af4aa
Charnjeet Sohal <charnsoh@imdb.com>
2026-05-16 19:25:39 +0100

    Initial set up of project structure and CI pipeline

---
+++ src/test/java/digitalid/PlaceholderTest.java
@@ -0,0 +1,12 @@
+package digitalid;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlaceholderTest {
+
+    @Test
+    void systemStarts() {
+        assertTrue(true);
+    }
+}
